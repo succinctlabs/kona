7706cb92672b87d324d2fc4a79f1b08d41760d47
Zach Obront <zobront@gmail.com>
2024-06-17 10:25:31 -0500

    fixes to get zkvm compiling again

--- bin/programs/sp1/src/main.rs
+++ bin/programs/sp1/src/main.rs
@@ -5,20 +5,13 @@ sp1_zkvm::entrypoint!(main);
 
 use alloy_consensus::{Header, Sealable};
 use alloy_rlp::Decodable;
-use kona_client::l2::StatelessL2BlockExecutor;
-use kona_derive::types::{L2PayloadAttributes, RollupConfig};
+use kona_executor::StatelessL2BlockExecutor;
+use kona_derive::types::{L2PayloadAttributes, OP_MAINNET_CONFIG};
 use kona_zkvm::{ZkvmTrieDBFetcher, ZkvmTrieDBHinter};
-use std::sync::Arc;
 
 pub fn main() {
-    let rollup_config = RollupConfig {
-        l2_chain_id: 10,
-        regolith_time: Some(0),
-        canyon_time: Some(0),
-        delta_time: Some(0),
-        ecotone_time: Some(0),
-        ..Default::default()
-    };
+    let rollup_config = OP_MAINNET_CONFIG;
+
     let raw_header = sp1_zkvm::io::read_vec();
     let header = Header::decode(&mut &raw_header[..]).unwrap();
 
@@ -34,7 +27,7 @@ pub fn main() {
     let payload_attrs: L2PayloadAttributes = sp1_zkvm::io::read();
 
     let mut l2_block_executor =
-        StatelessL2BlockExecutor::new(Arc::new(rollup_config), header.seal_slow(), fetcher, hinter);
+        StatelessL2BlockExecutor::new(&rollup_config, header.seal_slow(), fetcher, hinter);
     println!("Initialized block executor.");
     let produced_header = l2_block_executor.execute_payload(payload_attrs).unwrap().clone();
     println!("Executed payload.");
--- bin/zkvm-host/src/main.rs
+++ bin/zkvm-host/src/main.rs
@@ -3,10 +3,9 @@
 use alloy_consensus::{Header, Sealable};
 use alloy_primitives::{address, b256, hex};
 use alloy_rlp::Decodable;
-use kona_client::l2::StatelessL2BlockExecutor;
-use kona_derive::types::{L2PayloadAttributes, RollupConfig};
+use kona_executor::StatelessL2BlockExecutor;
+use kona_derive::types::{L2PayloadAttributes, RollupConfig, OP_MAINNET_CONFIG};
 use kona_zkvm::{ZkvmTrieDBFetcher, ZkvmTrieDBHinter};
-use std::sync::Arc;
 
 use sp1_sdk::{utils, ProverClient, SP1Stdin};
 
@@ -17,18 +16,11 @@ fn main() {
     // crate.
     // TODO: This calls relative to being in this directory, which fails if we call `cargo --bin zkvm-host` from root.
     let testdata_folder = "block_120794432_exec";
-    let file_name = format!("../programs/client/testdata/{}/output.json", testdata_folder);
+    let file_name = format!("../../crates/executor/testdata/{}/output.json", testdata_folder);
     let fetcher = ZkvmTrieDBFetcher::from_file(&file_name);
 
     // Make a mock rollup config, with Ecotone activated at timestamp = 0.
-    let rollup_config = RollupConfig {
-        l2_chain_id: 10,
-        regolith_time: Some(0),
-        canyon_time: Some(0),
-        delta_time: Some(0),
-        ecotone_time: Some(0),
-        ..Default::default()
-    };
+    let rollup_config = OP_MAINNET_CONFIG;
 
     // Decode the headers.
     let raw_header = hex!("f90244a0ff7c6abc94edcaddd02c12ec7d85ffbb3ba293f3b76897e4adece57e692bcc39a01dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347944200000000000000000000000000000000000011a0a0b24abb13d6149947247a8817517971bb8d213de1e23225e2b20d36a5b6427ca0c31e4a2ada52ac698643357ca89ef2740d384076ef0e17b653bcb6ea7dd8902ea09f4fcf34e78afc216240e3faa72c822f8eea4757932eb9e0fd42839d192bb903b901000440000210068007000000940000000220000006000820048404800002000004040100001b2000008800001040000018280000400001200004000101086000000802800080004008010001080000200100a00000204840000118042080000400804001000a0400080200111000000800050000020200064000000012000800048000000000101800200002000000080008001581402002200210341089000080c2d004106000000018000000804285800800000020000180008000020000000000020103410400000000200400008000280400000100020000002002000021000811000920808000010000000200210400000020008000400000000000211008808407332d3f8401c9c3808327c44d84665a343780a0edba75784acf3165bffd96df8b78ffdb3781db91f886f22b4bee0a6f722df93988000000000000000083202ef8a056e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b4218080a0917693152c4a041efbc196e9d169087093336da96a8bb3af1e55fce447a7b8a9");
@@ -41,7 +33,7 @@ fn main() {
 
     // Initialize the block executor on block #120794431's post-state.
     let mut l2_block_executor = StatelessL2BlockExecutor::new(
-        Arc::new(rollup_config),
+        &rollup_config,
         header.seal_slow(),
         fetcher.clone(),
         hinter,
