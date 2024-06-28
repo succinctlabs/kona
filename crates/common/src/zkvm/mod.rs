503dd55158d7f5b3b63f7607fab638feb4316034
Zach Obront <zobront@gmail.com>
2024-06-28 13:45:08 -0500

    reimplement zkvm io

---
+++ crates/common/src/zkvm/mod.rs
@@ -0,0 +1,4 @@
+//! This module contains raw syscall bindings for the `zkvm` target os, as well as a
+//! high-level implementation of the [crate::BasicKernelInterface] trait for the `asterisc` kernel.
+
+pub(crate) mod io;
