c255e44a7c9442051ef1a8176c6969941ed3db82
Zach Obront <zobront@gmail.com>
2024-07-10 20:16:11 -0700

    no io feature flag

--- crates/common/src/zkvm/mod.rs
+++
@@ -1,4 +0,0 @@
-//! This module contains raw syscall bindings for the `zkvm` target os, as well as a
-//! high-level implementation of the [crate::BasicKernelInterface] trait for the `asterisc` kernel.
-
-pub(crate) mod io;
