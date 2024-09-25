ad0371151c9e02d47f810b9280317808ccec998e
ratankaliani <ratan@succinct.xyz>
2024-09-25 02:56:50 +0000

    native client bugged

--- bin/host/src/lib.rs
+++ bin/host/src/lib.rs
@@ -121,14 +121,7 @@ pub async fn start_server_and_native_client(cfg: HostCli) -> Result<i32> {
             debug!(target: "kona_host", "Client program has exited with status {exit_status}.");
         }
     );
-    // tokio::spawn(async move {
-    //     start_native_preimage_server(kv_store, fetcher, hint_pipe.host, preimage_pipe.host)
-    //         .await
-    //         .unwrap();
-    // });
 
-    // let exit_status =
-    //     start_native_client_program(cfg, hint_pipe.client, preimage_pipe.client).await?;
     debug!(target: "kona_host", "Client program has exited with status {exit_status}.");
     info!(target: "kona_host", "Preimage server and client program have joined.");
 
