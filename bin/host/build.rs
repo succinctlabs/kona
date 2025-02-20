96dcf1dff913cb9bcd5e9df1b5512c79ac060e84
leruaa <aurelien.catinon@gmail.com>
2025-02-20 08:26:23 -0800

    fix: build.rs without main()

--- bin/host/build.rs
+++ bin/host/build.rs
@@ -1,9 +1,6 @@
 //! Builds the SP1 program
 
-#[cfg(feature = "sp1")]
-use sp1_helper::build_program;
-
-#[cfg(feature = "sp1")]
 fn main() {
-    build_program("./src/sp1/program");
+    #[cfg(feature = "sp1")]
+    sp1_helper::build_program("./src/sp1/program");
 }
